67f5b17c931c67a900fe395ea3f1971904254d17
Srujan Bharadwaj <srujanbharadwaj44@gmail.com>
2020-06-24 19:29:32 +0530

    Modify app.py

--- app.py
+++ app.py
@@ -1,85 +1,29 @@
-import urllib.request
-from bs4 import BeautifulSoup
-import re
 import os
+from bot import get_problem_urls, make_solution_files, create_io_files
 
-contestName = input("contest number: ")
+# ContestName is the number in the Codeforces contest URL
+# For example if the contest URL is https://codeforces.com/contest/1366, then contestName is 1366
 
-def problemsURLs(contestName):
-    contestURL = "https://codeforces.com/contest/" + contestName
-    fp = urllib.request.urlopen(contestURL)
-    mybytes = fp.read()
-
-    HTMLData = mybytes.decode("utf8")
-    fp.close()
-    soup = BeautifulSoup(HTMLData, "html.parser")
-    hrefs = []
-    for link in soup.find_all("a"):
-        href = link.get("href")
-        if(re.search("^/contest/" + contestName + "/problem/", href) and href not in hrefs):
-            hrefs.append(href)
-
-    return hrefs
-
-
-def makeSolutionFiles(fileNames, template):
-    for i in fileNames:
-        try:
-            os.mkdir("./" + contestName + "/" + i)
-            fp = open(contestName + "/" + i + "/sol.cpp", "w")
-            # fp = open(contestName + "/" + i + ".cpp", "w")
-            fp.write(template)
-        except:
-            print(i, " already exists")
-
-    print("All solution files generated")
-    print("Now gathering all solutions file")
-
-
-def createIOfiles(problemListsURL):
-        
-    for i in range(len(problemListsURL)):
-        problemListsURL[i] = "https://codeforces.com" + problemListsURL[i]
-
-    for problem in problemListsURL:
-        fp = urllib.request.urlopen(problem)
-        mybytes = fp.read()
-        problemPage = mybytes.decode("utf8")
-        fp.close()
-
-        IOSoup = BeautifulSoup(problemPage, "html.parser")
-
-        io = IOSoup.find_all("pre")
-
-
-        numberOfIOs = len(io)
-
-        words = problem.split("/")
-        problemCode = words[len(words) - 1]
-
-        for i in range(numberOfIOs//2):
-            inputText = io[i].get_text()
-            outputText = io[i + 1].get_text()
-            Ifile = open(contestName + "/" + contestName + problemCode + "/input" + str(i) + ".txt", "w")
-            Ifile.write(inputText)
-            Ofile = open(contestName + "/" + contestName + problemCode + "/output" + str(i) + ".txt", "w")
-            Ofile.write(outputText)
-
+contest_id = input("contest ID/number in the contest URL: ")
 
 try:
-    os.mkdir("./" + contestName)
-except:
+    os.mkdir("./" + contest_id)
+except OSError:
     print("Folder already exists")
 
-problemListsURL = problemsURLs(contestName) 
-template= open("./template.cpp", "r").read()
+problem_urls = get_problem_urls(contest_id)
+template = open("./template.cpp", "r").read()
 
-fileNames = []
-for problem in problemListsURL:
-    a = problem.split("/")
-    fileNames.append(contestName + a[len(a) - 1])
-    
-makeSolutionFiles(fileNames, template)
+problem_folder_names = []
 
+# problem_urls -> /contest/contest_id/problem/letter(A-Z)
+# Create problem names as contest_id + problem_code. Like 1336A
+for url in problem_urls:
+    url_split = url.split("/")
+    problem_folder_names.append(contest_id + url_split[len(url_split) - 1])
 
-createIOfiles(problemListsURL)
+# Create problem folders and make solution files for all problems with template
+make_solution_files(problem_folder_names, template, contest_id)
+
+# Create testcases files inside problem folders
+create_io_files(problem_urls, contest_id)
